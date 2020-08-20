ec2a87adf2466031cc61d758c7ec685f90ffb623
John Leimgruber <leimgrub@gmail.com>
2020-08-20 18:02:23 -0400

    Add rough restart logic to start.sh script
    Will restart every 5 seconds up to 10 times per manual start.
    
    Not great design, but quick bandage for this weekend away lol

--- start.sh
+++ start.sh
@@ -1,28 +1,39 @@
 #!/usr/bin/env bash
-# Change -Xmx to be less than actal sever RAM
-# Change -jar to match actual server version
+## Change -Xmx to be less than actal sever RAM
+## Change -jar to match actual server version
 
-java -Xms1G \
-     -Xmx3G \
-     -XX:+UseG1GC \
-     -XX:+ParallelRefProcEnabled \
-     -XX:MaxGCPauseMillis=200 \
-     -XX:+UnlockExperimentalVMOptions \
-     -XX:+DisableExplicitGC \
-     -XX:-OmitStackTraceInFastThrow \
-     -XX:+AlwaysPreTouch  \
-     -XX:G1NewSizePercent=30 \
-     -XX:G1MaxNewSizePercent=40 \
-     -XX:G1HeapRegionSize=8M \
-     -XX:G1ReservePercent=20 \
-     -XX:G1HeapWastePercent=5 \
-     -XX:G1MixedGCCountTarget=8 \
-     -XX:InitiatingHeapOccupancyPercent=15 \
-     -XX:G1MixedGCLiveThresholdPercent=90 \
-     -XX:G1RSetUpdatingPauseTimePercent=5 \
-     -XX:SurvivorRatio=32 \
-     -XX:MaxTenuringThreshold=1 \
-     -Dusing.aikars.flags=true \
-     -Daikars.new.flags=true \
-     -jar Mohist-1.12.2-5d032f7-server.jar \
-     nogui
+## This isn't great design, but quick bandage for now:
+## How many times to restart server before giving up
+COUNT=10
+
+while [ $COUNT -gt 0 ]; do
+    let COUNT=COUNT-1
+
+    java -Xms1G \
+         -Xmx3G \
+         -XX:+UseG1GC \
+         -XX:+ParallelRefProcEnabled \
+         -XX:MaxGCPauseMillis=200 \
+         -XX:+UnlockExperimentalVMOptions \
+         -XX:+DisableExplicitGC \
+         -XX:-OmitStackTraceInFastThrow \
+         -XX:+AlwaysPreTouch  \
+         -XX:G1NewSizePercent=30 \
+         -XX:G1MaxNewSizePercent=40 \
+         -XX:G1HeapRegionSize=8M \
+         -XX:G1ReservePercent=20 \
+         -XX:G1HeapWastePercent=5 \
+         -XX:G1MixedGCCountTarget=8 \
+         -XX:InitiatingHeapOccupancyPercent=15 \
+         -XX:G1MixedGCLiveThresholdPercent=90 \
+         -XX:G1RSetUpdatingPauseTimePercent=5 \
+         -XX:SurvivorRatio=32 \
+         -XX:MaxTenuringThreshold=1 \
+         -Dusing.aikars.flags=true \
+         -Daikars.new.flags=true \
+         -jar Mohist-1.12.2-5d032f7-server.jar \
+         nogui
+
+    echo "[$(date)] Server stopped! Will restart $COUNT more times!"
+    sleep 5
+done
